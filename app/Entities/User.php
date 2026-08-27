f1f26dfb9a5ac62c878d4f5b3013b568ecfed427
SallwenGit <fauque.adam@gmail.com>
2026-08-27 14:46:52 +0200

    feat: init (#1)

---
+++ app/Entities/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Shield\Entities\User as ShieldUser;
+class User extends ShieldUser
+{
+    protected ?Player $player = null;
+
+    public function getPlayer(): ?Player {
+        if($this->player === null && !($this->attributes['id'])) {
+            $playerModel = model(PlayerModel::class);
+            $this->player = $playerModel->findByUser($this->attributes['id']);
+            if (!$this->player) {
+                $this->player->setUser($this);
+            }
+        }
+        return $this->player;
+    }
+
+    public function setPlayer(Player $player): self {
+        $this->player = $player;
+        return $this;
+    }
+}
